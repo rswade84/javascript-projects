bcd77f668970d124443d93e3b3824392c335c7da
rswade84 <renaldowade@gmail.com>
2024-06-17 18:06:08 -0500

    Chapter 5 & 6 exercises

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -2,10 +2,17 @@
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
-if (engineIndicatorLight === "green") {
-   console.log("engines have started");
-} else if (engineIndicatorLight === "green blinking") {
-   console.log("engines are preparing to start");
+let engineIndicatorLight = 'red blinking';
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
+if (engineIndicatorLight === 'green') {
+  console.log('engines have started');
+} else if (engineIndicatorLight === 'green blinking') {
+  console.log('engines are preparing to start');
 } else {
-   console.log("engines are off");
+  console.log('engines are off');
 }
