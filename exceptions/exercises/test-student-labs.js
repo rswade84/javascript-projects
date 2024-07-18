181f15f127b123451d78b3177f563cf1aa937d80
rswade84 <renaldowade@gmail.com>
2024-07-18 12:10:25 -0500

    Exceptions Exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,35 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  for (let i = 0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+    try {
+      result = lab.runLab(3);
+    } catch (error) {
+      result = 'Error Thrown';
+    }
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
 
-let studentLabs = [
+let studentLabs2 = [
   {
-    student: 'Carly',
-    runLab: function (num) {
-        return Math.pow(num, num);
-    }
+    student: 'Blake',
+    myCode: function (num) {
+      return Math.pow(num, num);
+    },
   },
   {
-    student: 'Erica',
+    student: 'Jessica',
     runLab: function (num) {
-        return num * num;
-    }
-  }
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: 'Mya',
+    runLab: function (num) {
+      return num * num;
+    },
+  },
 ];
 
-gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
