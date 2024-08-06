359892122c1acd0ae625709fa98d9d06d0366812
rswade84 <renaldowade@gmail.com>
2024-08-05 19:15:01 -0500

    DOM & Events Exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -5,6 +5,25 @@ function init () {
 
     // Put your code for the exercises here.
     
+    button.addEventListener('click', function (event) {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener('mouseover', function (event) {
+        missionAbort.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseout', function (event) {
+        missionAbort.style.backgroundColor = '';
+    })
+
+    missionAbort.addEventListener('click', function (event) {
+        let confirmAction = confirm('Are you sure you want to abort the mission?');
+        if (confirmAction) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+        }
+
+    })
 }
 
 window.addEventListener("load", init);
